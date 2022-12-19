b3a62cf00d2753ba6a2f552765f4fce2306e4ebf
James Howard <jamh885@gmail.com>
2022-12-19 14:49:28 -0600

    don't

--- ContentView.swift
+++ ContentView.swift
@@ -1,17 +1,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var myname:String = ""
     var body: some View {
         VStack {
-            TextField("hello", text: $myname)
-           Text("\(myname)")
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("James")
-            Text("Hello")
-        Text("James is not an idiot")
+          Image("hwxtm6n79ma61")
+                .resizable()
+                .scaledToFit()
             
         }
     }

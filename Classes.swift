092b6d6aaa7faa8baa8058705f635847fe8bc24c
James Howard <jamh885@gmail.com>
2022-12-19 14:36:56 -0600

    added functions

--- Classes.swift
+++ Classes.swift
@@ -7,6 +7,164 @@ import Foundation
 import SwiftUI
 import Swift
 
-class someclass {
-    
+    func Card1 (Flipped:Bool) {
+        if Flipped == true {
+            
+        }
+        else{
+            Image("Globe")
+        }
+    }
+
+func Card2 (Flipped2:Bool) {
+    if Flipped2 == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
+}
+func Card3 (Flipped3:Bool) {
+    if Flipped3 == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
+}
+func Card4 (Flipped4:Bool) {
+    if Flipped4 == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
+}
+func Card5 (Flipped5:Bool) {
+    if Flipped5 == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
+}
+func Card6 (Flipped6:Bool) {
+    if Flipped6 == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
+}
+func Card7 (Flipped7:Bool) {
+    if Flipped7 == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
+}
+func Card8 (Flipped8:Bool) {
+    if Flipped8 == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
+}
+func Card9 (Flipped9:Bool) {
+    if Flipped9 == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
+}
+func Card10 (Flipped10:Bool) {
+    if Flipped10 == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
+}
+func Card11 (Flipped11:Bool) {
+    if Flipped11 == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
+}
+func Card12 (Flipped12:Bool) {
+    if Flipped12 == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
+}
+func Card13 (Flipped13:Bool) {
+    if Flipped == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
+}
+func Card14 (Flipped14:Bool) {
+    if Flipped14 == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
+}
+func Card15 (Flipped15:Bool) {
+    if Flipped15 == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
+}
+func Card16 (Flipped16:Bool) {
+    if Flipped16 == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
+}
+func Card17 (Flipped17:Bool) {
+    if Flipped17 == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
+}
+func Card18 (Flipped18:Bool) {
+    if Flipped18 == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
+}
+func Card19 (Flipped19:Bool) {
+    if Flipped19 == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
+}
+func Card20 (Flipped20:Bool) {
+    if Flipped20 == true {
+        
+    }
+    else{
+        Image("Globe")
+    }
 }

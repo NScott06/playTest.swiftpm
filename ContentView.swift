5585e6c458e493aea9b7d3a5f609cb41554682af
Nicholas Scott <nscott4525@stu.d214.org>
2022-12-19 14:49:46 -0600

    added filmcard animation

--- ContentView.swift
+++ ContentView.swift
@@ -1,18 +1,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var myname:String = ""
+    //MARK: Variables
+    @State var backDegree = 0.0
+    @State var frontDegree = -90.0
+    @State var isFlipped = false
+
+    let width : CGFloat = 200
+    let height : CGFloat = 250
+    let durationAndDelay : CGFloat = 0.3
+
+    //MARK: Flip Card Function
+    func flipCard () {
+        isFlipped = !isFlipped
+        if isFlipped {
+            withAnimation(.linear(duration: durationAndDelay)) {
+                backDegree = 90
+            }
+            withAnimation(.linear(duration: durationAndDelay).delay(durationAndDelay)){
+                frontDegree = 0
+            }
+        } else {
+            withAnimation(.linear(duration: durationAndDelay)) {
+                frontDegree = -90
+            }
+            withAnimation(.linear(duration: durationAndDelay).delay(durationAndDelay)){
+                backDegree = 0
+            }
+        }
+    }
+    //MARK: View Body
     var body: some View {
-        VStack {
-            TextField("hello", text: $myname)
-           Text("\(myname)")
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("James")
-            Text("Hello")
-        Text("James is not an idiot")
-            
+        ZStack{
         }
     }
 }
